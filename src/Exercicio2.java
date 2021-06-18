a7d6622c94f04d7a31682d5905dd0252bf9ee483
Ricardo <ricardo_brito_santos@hotmail.com>
2021-06-17 21:14:21 -0300

    Vetores, Exercicio2 Resolvido

--- src/Exercicio2.java
+++ src/Exercicio2.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 
-import com.sun.tools.javac.code.Type.ForAll;
 
 //O professor Wuni Jim Yensonal precisa de um programa para ajudar  com  um  problema  simples:  para  uma  turma  com  50  alunos precisa calcular a média da sala e descobrir quais são os alunos que precisarão  de  um  atendimento personalizado.  O  fluxo  do  programa deve ser o seguinte: A.Digitar as 50 notasem um vetor e, para cada nota digitada, digitar o nome do aluno correspondente em outro vetorB.Calcular a média das notas digitadasC.Para cada uma das notas que foi digitada e ficou abaixo da média, exibir uma mensagem que diga “Chamar o aluno para conversar”
 
@@ -18,8 +17,20 @@ public class Exercicio2 {
 			System.out.print("Digitar a " + (i+1) + "º nota: ");
 			notas[i] = scanner.nextDouble();
 			System.out.print("Digitar o nome do aluno que recebeu essa nota: ");
-			alunos[i] = scanner.next();			
+			alunos[i] = scanner.next();
+			
+			mediaNotas += notas[i]; 
 		}
+		
+		mediaNotas /= notas.length;
+		
+		for (int i = 0; i < alunos.length; i++) {
+			if (notas[i] < mediaNotas ) {
+				System.out.println("Chamar o aluno "+ alunos[i]+" para conversar");
+			}
+		}
+		
+		scanner.close();
  
 	}
 
